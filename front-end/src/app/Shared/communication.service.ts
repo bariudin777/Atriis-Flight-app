86e874d1ad926cbc02846ffcc3c31ee958c2deb7
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2022-04-29 23:02:30 +0300

    created service comunication

---
+++ front-end/src/app/Shared/communication.service.ts
@@ -0,0 +1,17 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class CommunicationService {
+  noAuthHeader = { headers: new HttpHeaders({ 'NoAuth': 'True' }) };
+  constructor(private http: HttpClient) { }
+
+  getFlights(flightReq: any) {
+    return this.http.post(environment.apiBaseUrl+'getFlights',flightReq,this.noAuthHeader)
+  }
+
+}
