c768ea3e7c30e5a49514837aaa412a16834cddea
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2022-04-30 23:17:14 +0300

    data import

--- front-end/src/app/services/data.service.ts
+++ front-end/src/app/services/data.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { timeout } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { FlightsData } from './Models/flights-data.model';
 
 @Injectable({
   providedIn: 'root'
